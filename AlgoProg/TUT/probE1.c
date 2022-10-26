8bbe5b64d66e44872a0c05b7a28adbafba8d142b
ihzacky <114144242+ihzacky@users.noreply.github.com>
2022-10-26 19:29:26 +0700

    array improvement from probE

---
+++ AlgoProg/TUT/probE1.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+int T;
+int X[100];
+long int arrayNum[100][100];
+int sum[100][100];
+
+int main(){
+
+    scanf("%d", &T); // input testcase loop
+
+    /*------ TESTCASE LOOP ------*/
+    for (int i = 0; i < T; i++){
+
+        /*-------- INPUT --------*/
+        scanf("%d", &X[i]);
+
+        for (int j = 0; j < X[i]; j++){
+            for (int k = 0; k < X[i]; k++){
+
+                scanf("%d", &arrayNum[j][k]);
+                
+            }
+        }
+
+        /*------- PROCESS -------*/
+        for (int row = 0; row < X[i]; row++){
+
+            sum[i][row] = 0;
+            for (int col = 0; col < X[i]; col++){
+                
+                sum[i][row] += arrayNum[col][row];
+                
+            }
+        }
+        
+    }
+    
+    /*-------- OUTPUT --------*/
+    for (int i = 0; i < T; i++){
+
+        printf("Case #%d: ", i+1);
+        for (int p = 0; p < X[i]; p++){
+
+            printf("%d", sum[i][p]);
+            if(p == X[i]-1){
+                printf("\n");
+            } else {
+                printf(" ");
+            }
+        }
+
+    }
+
+    return 0;
+}
