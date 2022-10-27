5cf7d2f898f63d9d750632ee44c2257f4c4a4967
ihzacky <114144242+ihzacky@users.noreply.github.com>
2022-10-27 08:46:57 +0700

    done

--- AlgoProg/LEC/session_9-10/exercise2.c
+++ AlgoProg/LEC/session_9-10/exercise2.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
+#include <string.h>
 
-
+char string[100];
+int length;
+char output[100];
 
 int main(){
 
+    scanf("%s", string);
 
+    length = strlen(string);
+
+    for (int i = 0; i < length; i++){
+
+        output[(length-1)-i] = string[i];        
+        
+    }
+    
+    printf("%s", output);
 
     return 0;
 }
