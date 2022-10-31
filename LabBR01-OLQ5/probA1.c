8edda881fa28e400b080a8568e4b3207275fea49
ihzacky <114144242+ihzacky@users.noreply.github.com>
2022-10-31 22:21:24 +0700

    fp for online judge

--- LabBR01-OLQ5/probA1.c
+++ LabBR01-OLQ5/probA1.c
@@ -13,11 +13,7 @@ int main(){
         {
             for (int j = 1; j <= N; j++)
             {
-                if
-                (   i == 1 || i == N ||
-                    j == 1 || j == N ||
-                    i == j || j == (N - i + 1)
-                ){
+                if(i == 1 || i == N || j == 1 || j == N || i == j || j == (N - i + 1)){
                     printf("*");
                 } else{
                     printf(" ");
@@ -26,6 +22,8 @@ int main(){
 
             printf("\n");
         }
+
+        printf("\n");
     }
     
     return 0;
