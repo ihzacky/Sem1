ad860e87118c3a978182bf533354d32f075766ea
ihzacky <114144242+ihzacky@users.noreply.github.com>
2022-10-31 20:35:23 +0700

    done

--- LabBR01-OLQ5/probD.c
+++ LabBR01-OLQ5/probD.c
@@ -1,12 +1,34 @@
 #include <stdio.h>
 
-
+int N;
+int A[1000];
+int steps;
 
 int main(){
 
+    scanf("%d", &N);
+    getchar();
 
-
+    for (int i = 0; i < N; i++){
+        scanf("%d", &A[i]);
+    }
     
+    int max = A[0]; 
+    for (int i = 0; i < N; i++){ 
+        
+        if(A[i] > max){ 
+            max = A[i];
 
+        }
+        if (A[i+1] == 1){
+            printf("%d ", max);
+            max = 0;
+
+        }
+        
+    }
+    
+    printf("%d\n", A[N-1]);
+    
     return 0;
 }
