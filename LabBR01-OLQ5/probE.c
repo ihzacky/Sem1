3026a350ee76b08510020546e8da7ebe2903d76e
ihzacky <114144242+ihzacky@users.noreply.github.com>
2022-11-01 20:09:17 +0700

    done, green

--- LabBR01-OLQ5/probE.c
+++ LabBR01-OLQ5/probE.c
@@ -2,18 +2,19 @@
 
 int T;
 int N;
-double A[100]; // number inputted
-int P;  // bibi's index
-int Q;  // lili's index
+int P;  
+int Q;  
 
 int main(){
 
-    scanf("%d", &T);    // testcase
+    scanf("%d", &T);    
     for (int i = 0; i < T; i++){
 
-        scanf("%d", &N);    // how many number inputted
+        scanf("%d", &N);    
+        int A[10000]; 
+        
         for (int j = 0; j < N; j++){
-            scanf("%lf", &A[j]);    // input the number
+            scanf("%d", &A[j]);    
         }
         
         scanf("%d %d", &P, &Q);
@@ -28,8 +29,7 @@ int main(){
         } else if (A[P] == A[Q]){
             printf("Draw\n");
         }
-        // printf("bibi= %.0lf\n", A[P]);
-        // printf("lili= %.0lf\n", A[Q]);
+
     }
 
     
