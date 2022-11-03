daa3e3a71d748aa582b052769b73fd901deafc4f
ihzacky <114144242+ihzacky@users.noreply.github.com>
2022-11-03 23:19:40 +0700

    done

--- LabBR01-OLQ6/probF.c
+++ LabBR01-OLQ6/probF.c
@@ -1,11 +1,62 @@
 #include <stdio.h>
+#include <string.h>
 
+char str[1000000];
+char voc[21] = {'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 
+                'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'};        
+char cons[5] = {'a', 'e', 'i', 'o', 'u'};
+int len;
 
+int vocals[21];
+int consonants[5];
+
+int vocCount;
+int consCount;
 
 int main(){
 
+    scanf("%[^\n]", str);
 
+    len = strlen(str);
+    for (int i = 0; i < len; i++){
 
+        for (int j = 0; j < 21; j++){
+
+            if (str[i] == voc[j])
+            {
+                vocals[j]++;
+            }
+            
+        }
+
+        for (int p = 0; p < 5; p++){
+
+            if (str[i] == cons[p])
+            {
+                consonants[p]++;
+            }
+        }
+        
+    }
+        
+    for (int j = 0; j < 21; j++){
+        
+        if (vocals[j] > 0){
+        vocCount++;
+        }
+        
+    }
+
+    for (int k = 0; k < 5; k++){
+        
+        if (consonants[k] > 0){
+        consCount++;
+        }
+
+    }
+
+    printf("Vocal: %d\n", consCount);
+    printf("Consonant: %d\n", vocCount);
     
 
     return 0;
