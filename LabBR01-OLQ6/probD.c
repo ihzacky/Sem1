dd252ba5c4284bcf1775065402f839a52be819ac
ihzacky <114144242+ihzacky@users.noreply.github.com>
2022-11-08 11:44:52 +0700

    done

--- LabBR01-OLQ6/probD.c
+++ LabBR01-OLQ6/probD.c
@@ -1,12 +1,41 @@
 #include <stdio.h>
 
+int T;
+int N;
+int A, B, C, D;
 
+// ini disuruh cari fpb dari 4 angka
 
 int main(){
 
+    scanf("%d", &T);
+    int result[T];
 
+    for (int i = 0; i < T; i++){
+       
+        scanf("%d", &N);
+        getchar();
 
+        scanf("%d %d %d %d", &A, &B, &C, &D);
+        getchar();
+
+        int count = 0;
+        for (int j = 1; j <= N; j++){
+        
+        if (j%A == 0 || j%B == 0 || j%C == 0 || j%D == 0){
+
+            count++;
+        }
+
+       }
+       
+        result[i] = count;
+    }
     
+    for (int i = 0; i < T; i++){
+
+        printf("Case #%d: %d\n", i+1, result[i]);
+    }
 
     return 0;
 }
