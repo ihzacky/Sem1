d42c61cc99cca355b69114e4c5df038db3948cbc
ihzacky <114144242+ihzacky@users.noreply.github.com>
2022-11-08 11:45:37 +0700

    cleaned

--- LabBR01-OLQ5/probC1.c
+++ LabBR01-OLQ5/probC1.c
@@ -19,42 +19,6 @@ int main(){
 
         strLen = strlen(str);
         for (int i = 0; i < strLen; i++){
-            
-            // switch (str[i])
-            // {
-            // case 0:
-            //     str[i] = 'O';
-            //     break;
-            // case 1:
-            //     str[i] = 'I';
-            //     break;
-            // case 2:
-            //     str[i] = 'Z';
-            //     break;
-            // case 3:
-            //     str[i] = 'E';
-            //     break;
-            // case 4:
-            //     str[i] = 'A';
-            //     break;
-            // case 5:
-            //     str[i] = 'S';
-            //     break;
-            // case 6:
-            //     str[i] = 'G';
-            //     break;
-            // case 7:
-            //     str[i] = 'T';
-            //     break;
-            // case 8:
-            //     str[i] = 'B';
-            //     break;
-            // case 9:
-            //     str[i] = 'P';
-            //     break; 
-            // default:
-            //     break;
-            // }
 
             switch (str[i])
             {
