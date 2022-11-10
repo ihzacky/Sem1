2141c3d4dfaeb08e49fb8d4050e005395a4b8f9d
ihzacky <114144242+ihzacky@users.noreply.github.com>
2022-11-10 21:39:57 +0700

    done, green

--- LabBR01-OLQ7/probC.c
+++ LabBR01-OLQ7/probC.c
@@ -1,10 +1,29 @@
 #include <stdio.h>
 
+int T;
 
+double A, B, C;
 
 int main(){
 
+    scanf("%d", &T);
+    getchar();
 
+    for (int i = 0; i < T; i++){
+
+        scanf("%lf %lf %lf", &A, &B, &C);
+        getchar();
+
+        if ((A <= B <= C) && (A + B > C)){
+            
+            printf("BISA\n");
+            
+        } else{
+
+            printf("TIDAK BISA\n");
+        }
+
+    }
 
     return 0;
 }
