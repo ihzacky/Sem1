b1d9f8c51010725ead5ea42d9e1500db3464cbbf
ihzacky <114144242+ihzacky@users.noreply.github.com>
2022-11-13 23:25:17 +0700

    added more function

--- PDM/PDM_TUT_session3.c
+++ PDM/PDM_TUT_session3.c
@@ -15,17 +15,18 @@ void transfer();
 
 void add();
 void modify();
-void delete();
+void del();
 
 int main(){
 
     // system("cls");
     while (menu_selector != 3){
 
-        printf("1. List rekening\n");
-        printf("2. Transfer dana\n");
-        printf("3. Exit\n");
-        printf(">> ");
+        printf("[-------------------]\n");
+        printf("  1. Account list\n");
+        printf("  2. Transfer funds\n");
+        printf("  3. Exit\n");
+        printf("\n>> ");
         scanf("%d", &menu_selector);
         getchar();
 
@@ -70,37 +71,7 @@ void list(){
     int check = getc(fptr);
     if (check == EOF){
         
-        fptr = fopen("list_rekening.txt", "w");
-
-        printf("No data!\nPlease add account destination\n");
-        
-        x:
-        printf("Account Number [Max: 5 number]: ");
-        scanf("%s", rekening.no);
-        getchar();
-        for (int p = 0; p < strlen(rekening.no); p++){
-
-            if (strlen(rekening.no) > 5){
-
-                goto x;
-            }
-            if (!(rekening.no[p] >= '1' && rekening.no[p]<= '9')){
-
-                goto x; 
-            }
-
-        }
-
-        printf("Account Name: ");
-        scanf("%[^\n]", rekening.nama); 
-        getchar();
-
-        printf("%s", rekening.no);
-        fprintf(fptr, "%s_%s_0", rekening.no, rekening.nama);
-  
-        fclose(fptr);
-        system("cls");
-        return;
+        add();
     }
 
     fptr = fopen("list_rekening.txt", "r");
@@ -112,26 +83,62 @@ void list(){
 			lcount++;
         }    
 	}
-    lcount++;
     fclose(fptr);
 
     fptr = fopen("list_rekening.txt", "r");
-    printf("----- LIST OF ACCOUNT -----\n");
+    printf(">> ACCOUNT LIST\n");
     for (int i = 0; i < lcount; i++){
 
         printf("%d.", i+1);
-        fscanf(fptr, "%[^_]_%[^_]_%d", rekening.no, rekening.nama, rekening.balance);
-        printf("\tAccount Number: %s\n\tAccount Name: \t%s\n\n", rekening.no, rekening.nama);
+        fscanf(fptr, "%[^_]_%[^_]_%d\n", rekening.no, rekening.nama, rekening.balance);
+        printf("\tAccount Number\t: %s\n", rekening.no);
+        printf("\tAccount Name\t: %s\n\n", rekening.nama);
 
     }
     fclose(fptr);
 
-    printf("1. Add new account\n");
-    printf("2. Modify exsisting account\n");
-    printf("3. Delete exsisting account\n");
-    printf(">> ");
+    printf("[-----------------------------]\n");
+    printf("  1. Add new account\n");
+    printf("  2. Modify exsisting account\n");
+    printf("  3. Delete exsisting account\n");
+    printf("  4. Back to main menu\n");
+    printf("\n>> ");
     scanf("%d", &menu_selector); 
     getchar();
+
+    while (menu_selector != 4){
+    
+        switch (menu_selector)
+        {
+        case 1:
+            
+            system("cls");
+            add();
+            break;
+        case 2:
+
+            system("cls");
+            // modify();    
+            break;
+        case 3:
+
+            system("cls");
+            // del();
+            break;
+        case 4:
+
+            system("cls");
+            return;
+            break;
+        
+        default:
+
+            system("cls");
+            printf("Invalid command!\n");
+            break;
+        }
+
+    }
     
 }
 
@@ -139,4 +146,53 @@ void transfer(){
 
 
 
+}
+
+void add(){
+
+    FILE *fptr = fopen("list_rekening.txt", "a");
+    
+    int c = fgetc(fptr);
+    if (c == EOF) {
+
+        printf("No data!\nPlease add account destination\n");
+    } else {
+
+        ungetc(c, fptr);
+    }
+
+    // int check = getc(fptr);
+    // if (check == EOF){
+        
+    //     printf("No data!\nPlease add account destination\n");
+    // }
+    
+    x:
+    printf("Account Number [Max: 5 number]: ");
+    scanf("%s", rekening.no);
+    getchar();
+    for (int p = 0; p < strlen(rekening.no); p++){
+
+        if (strlen(rekening.no) > 5){
+
+            goto x;
+        }
+        if (!(rekening.no[p] >= '0' && rekening.no[p]<= '9')){
+
+            goto x; 
+        }
+
+    }
+
+    printf("Account Name: ");
+    scanf("%[^\n]", rekening.nama); 
+    getchar();
+
+    printf("%s", rekening.no);
+    fprintf(fptr, "%s_%s_0\n", rekening.no, rekening.nama);
+
+    fclose(fptr);
+    system("cls");
+
+    return list();
 }
