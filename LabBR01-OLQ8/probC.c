49d70f0f7448ad09ef34fe77700fa578daea4a2a
ihzacky <114144242+ihzacky@users.noreply.github.com>
2022-12-09 11:34:11 +0700

    done, not yet submitted to OJ

--- LabBR01-OLQ8/probC.c
+++ LabBR01-OLQ8/probC.c
@@ -1,11 +1,31 @@
+#include <stdio.h>
 
-
+int N; // ordo
 
 int main(){
 
+    scanf("%d", &N);
+    getchar();
+    
+    char photo[N][N];
+    char printed[N][N];
 
+    for (int i = 0; i < N; i++){
+        for (int j = 0; j < N; j++){
 
+            scanf("%c", &photo[i][j]);
+        }
 
+        getchar();
+    }
+    
+    for (int i = N - 1; i >= 0; i--){
+        for (int j = N - 1; j >= 0; j--){
+
+            printf("%c", photo[i][j]);
+        }
+        printf("\n");
+    }
 
     return 0;
 }
