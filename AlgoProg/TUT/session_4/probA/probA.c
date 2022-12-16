842f3255ff8225d4957fe0a1cf8dfc89ca3667b4
ihzacky <114144242+ihzacky@users.noreply.github.com>
2022-12-17 00:33:57 +0700

    done

--- AlgoProg/TUT/session_4/probA/probA.c
+++ AlgoProg/TUT/session_4/probA/probA.c
@@ -1,29 +1,58 @@
 #include <stdio.h>
+#include <string.h>
 
 int N;
-char M[40], P[40];
-int S;
+int T;
+
+struct ax{
+
+    char M[40];
+    char P[40];
+} read[100];
+
+struct input_test{
+
+    char S[40];
+} input[100];
 
 int main(){
 
-    // FILE *fp = ("testdata.txt", "w");
+    FILE *fp = fopen("testdata.in", "r");
 
-    // scanf("%d", &N);
-    // for (int i = 0; i < N; i++){
+    fscanf(fp, "%d\n", &N);
+    for (int i = 0; i < N; i++){
 
-    //     fprintf(fp, "%[^#]#%[^\n]", &M, &P);
-    //     getchar();
-    // }
-    
-    // fclose(fp);
+        fscanf(fp, "%[^#]#%[^\n]\n", &read[i].M, &read[i].P);
+    }
 
-    scanf("%d", &S);
-    for (int i = 0; i < S; i++){
+    fscanf(fp,"%d\n", &T);
+    for (int i = 0; i < T; i++){
        
-       
+       fscanf(fp, "%s", &input[i].S);
     }
     
+    fclose(fp);
 
+    for (int i = 0; i < T; i++){
+
+        printf("Case #%d: ", i+1);
+
+        int found = 0;
+        for (int j = 0; j < N; j++){
+
+            if (strcmp(input[i].S, read[j].M) == 0){
+
+                printf("%s\n", read[j].P);
+                found = 1;
+                break;
+            }
+        }
+
+        if (found == 0){
+            
+            printf("N/A\n");
+        }
+    }
 
     return 0;
 }
