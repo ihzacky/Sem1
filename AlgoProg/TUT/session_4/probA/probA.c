0877e622d029ceffa2f660c2b12c14dc76b9a3e7
ihzacky <114144242+ihzacky@users.noreply.github.com>
2022-12-16 23:26:47 +0700

    initial commit

---
+++ AlgoProg/TUT/session_4/probA/probA.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int N;
+char M[40], P[40];
+int S;
+
+int main(){
+
+    // FILE *fp = ("testdata.txt", "w");
+
+    // scanf("%d", &N);
+    // for (int i = 0; i < N; i++){
+
+    //     fprintf(fp, "%[^#]#%[^\n]", &M, &P);
+    //     getchar();
+    // }
+    
+    // fclose(fp);
+
+    scanf("%d", &S);
+    for (int i = 0; i < S; i++){
+       
+       
+    }
+    
+
+
+    return 0;
+}
