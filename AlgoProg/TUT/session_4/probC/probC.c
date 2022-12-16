6671a3bc79729266485e83b9e775da929ea86984
ihzacky <114144242+ihzacky@users.noreply.github.com>
2022-12-17 00:40:34 +0700

    done

--- AlgoProg/TUT/session_4/probC/probC.c
+++ AlgoProg/TUT/session_4/probC/probC.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
-
+long int A;
+long int B;
 
 int main(){
 
+    FILE *fp = fopen("testdata.in", "r");
 
-
+    fscanf(fp, "%d %d", &A, &B);
+    printf("%d\n", A + B);
 
     return 0;
 }
