b5dbb67d7c44f25db1b2c0533017ab135341b419
ihzacky <114144242+ihzacky@users.noreply.github.com>
2022-12-21 09:39:23 +0700

    half way

--- LabBR01-OLQ9/probE.c
+++ LabBR01-OLQ9/probE.c
@@ -1,11 +1,50 @@
 #include <stdio.h>
+#include <string.h>
 
+int T;
+int TC;
 
+struct read_kamus
+{
+    char kataPendek[50];
+    char kataAsli[50];
+} kamus[100];
+
+char read_tc[100];
+char read_temp[50];
 
 int main (){
 
+    FILE *fp = fopen("testdata.in", "r");
+    fscanf(fp, "%d\n", &T);
+    
+    for (int i = 0; i < T; i++){
 
+        fscanf(fp, "%[^#]#%[^\n]\n", kamus[i].kataPendek, kamus[i].kataAsli);
+        // printf("%s %s\n", kamus[i].kataPendek, kamus[i].kataAsli);
+    }
+    
+    fscanf(fp, "%d\n", &TC);
+    for (int i = 0; i < TC; i++){
 
+        fscanf(fp, "%[^\n]\n", read_tc);
+        int len = strlen(read_tc);
+        // printf("%d\n", len);
+
+        int counter = 0;
+        int space = 0;
+        for (int j = 0; j < len; j++){
+
+            if (read_tc[j] == 32){
+                
+                space++;
+            }
+            
+        }
+        
+
+    }
+    
 
     return 0;
 }
--- LabBR01-OLQ9/probF.c
+++ LabBR01-OLQ9/probF.c
@@ -1,11 +1,32 @@
 #include <stdio.h>
+#include <string.h>
 
+int T;
+int n;
 
+char x[16];
+char y[16];
+char z[16];
+char temp[16];
 
 int main (){
 
+    scanf("%d", &T); getchar();
+    for (int i = 0; i < T; i++){
 
+        scanf("%d %c %c",&n, &x, &y); getchar();
 
+        printf("Case #%d: ", i+1);
+        for (int j = 0; j < n; j++){
+
+            strcpy(z, x);/*copying x into z*/
+		    strcpy(x, y);/*copying y into x*/
+		    strcat(y, z);
+            
+        }
+
+        printf("%s\n", x);
+    }
 
     return 0;
 }
