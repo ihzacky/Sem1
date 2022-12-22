a8949023c2ce934981e486ce148787d80ae35d90
ihzacky <114144242+ihzacky@users.noreply.github.com>
2022-12-22 20:59:05 +0700

    done, green

--- LabBR01-OLQ10/probF.c
+++ LabBR01-OLQ10/probF.c
@@ -1,10 +1,25 @@
 #include <stdio.h>
+#include <math.h>
 
+#define PI 3.14
 
+long long int T;
+int R;
+int H;
+double result;
 
 int main(){
 
+    scanf("%lld", &T); getchar();
+    for (int i = 0; i < T; i++){
+        
+        scanf("%d %d", &R, &H); getchar();
 
+        result = (2 * PI * R * H) + (2 * PI * pow(R, 2));
+
+        printf("Case #%d: %.2lf\n", i+1, result);
+
+    }
 
     return 0;
 }
